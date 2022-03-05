782dbf8701e24af85b141e0fda484bbc2a5775dd
dwnlodpark12 <dwnlodpark12@gmail.com>
2022-03-05 23:54:00 +0900

    JS syntax Study - operator

--- operator.js
+++ operator.js
@@ -1,3 +1,4 @@
+'use strict'
 // 1. String concatnation
 console.log('my ' + 'cat'); // my cat type: string
 console.log('1' + 2); // 12 type: string
@@ -23,3 +24,64 @@ console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
 const postIncrement = counter++;
 // postIncrement = counter;
 // counter = counter + 1;
+
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y;  // x = x + y; 
+x -= y;  // x = x - y;
+x *= y;  // x = x * y;
+x /= y;  // x = x / y;
+
+// 5. Comparison operators
+console.log(10 < 6);  // less than // false
+console.log(10 <= 6); // less than or equal // false 
+console.log(10 > 6);  // greater than // true 
+console.log(10 >= 6); // greater than or equal // true 
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = false;
+const value2 = 4 < 2;
+
+// || (or), finds the first truthy value
+console.log(`or: ${value1 || value2 || check()}`);
+
+// && (and), finds the first falsy value
+console.log(`and: ${value1 && value2 && check()}`);
+
+// often used to compress long if-statement 
+// nullableObject && nullableObject.somthing
+
+function check() {
+  for(let i = 0; i < 10; i++) {
+    //wasting Time
+    console.log('😴');
+  }
+  return true;
+}
+
+// ! (not)
+console.log(!value1);
+
+// 7. Equality 
+const stringFive = '5';
+const numberFive = 5;
+
+// == loose equality, with type conversion
+console.log(stringFive == numberFive);  // true
+console.log(stringFive != numberFive);  // false
+
+// === strict equality, no type conversion
+console.log(stringFive === numberFive); // false
+console.log(stringFive !== numberFive); //true
+
+// object equality by reference
+const hyerin1 = {name: 'hyerin'};
+const hyerin2 = {name: 'hyerin'};
+const hyerin3 = hyerin1;
+
+console.log(hyerin1 == hyerin2);  //true
+console.log(hyerin1 === hyerin2); //false
+console.log(hyerin1 === hyerin3); // true
