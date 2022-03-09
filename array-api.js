5db87a08e494490177119bfe26f84835918438a3
dwnlodpark12 <dwnlodpark12@gmail.com>
2022-03-10 01:23:22 +0900

    array-api study fin.

--- array-api.js
+++ array-api.js
@@ -54,3 +54,68 @@ class Student {
   }
 }
 
+const students = [
+  new Student('A', 29, true, 45),
+  new Student('B', 28, false, 80),
+  new Student('C', 30, true, 90),
+  new Student('D', 40, false, 66),
+  new Student('E', 18, true, 72),
+];
+
+// Q5. find a student with the score 90
+{
+  const result = students.find(student => student.score == 90);
+  console.log(result);
+}
+
+// Q6. make an array of enrolled students 
+{
+  const result = students.filter(student => student.enrolled);
+  console.log(result);
+}
+
+// Q7. make an array containing only the students' scores
+// return should be: [45, 80, 90, 66, 72]
+{
+  const result = students.map(student => student.score); 
+  console.log(result);
+}
+
+// Q8. check if there is a student with the score lower than 50
+{
+  const result = students.some(student => student.score < 50);
+  console.log(result); // true , 조건이 하나 이상이라도 만족 한다면 true return
+
+  const result2 = students.every(student => student.score > 50);
+  console.log(result2); // false, 조건이 하나라도 만족하지 않는다면 false
+}
+// Q9. compute students' average score *************** 
+{
+  // reduce 
+  const result = students.reduce((prev, curr) => {
+    // console.log(prev);
+    // console.log(curr);
+
+    return prev + curr.score;
+  }, 0);
+  console.log(result / students.length);
+}
+
+// Q10. make a string containing all the scores 
+// result should be : '45, 66, 72, 80, 70'
+{
+  const result = students
+    .map(student => student.score)
+    .filter(score => score >= 50)
+    .join();
+  console.log(result);
+}
+
+// Bonus! do Q10 sorted in ascending order
+// result should be : '45, 66, 72, 80, 90'
+{
+  const result = students
+    .map(student => student.score)
+    .sort().join();
+  console.log(result);
+}
